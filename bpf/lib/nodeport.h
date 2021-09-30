eddbbbbe1764e1457573471f97b9342dd00c713e
Gobinath Krishnamoorthy <gobinathk@google.com>
2021-09-30 04:33:02 +0000

    Perform reverse NAT at Host Interface
    Description: if NETFILTER_COMPAT_MODE flag is set, guarantees that traffic will
    pass-through kernel netfilter and that Iptables rules are enforced on traffic.
    
    Problem: if Nodeport traffic ingress on the same node as a backend pod's node,
    NAT and reverse NAT translations at 2 different interfaces.
    On ingress path, NAT happens at Node's host interface, pass through kernel stack.
    on egress path reverse NAT happen at Pod's Veth interface and bypasses kernel stack.
    This asymmetric behavior, reflects in kernel conntrack entries and having a IPTABLE
    rule to DROP any packets if conntrack state is INVALID which results in packet DROP.
    
    Fixes: #11914
    
    Signed-off-by: Gobinath Krishnamoorthy <gobinathk@google.com>
    Signed-off-by: Weilong Cui <cuiwl@google.com>

--- bpf/lib/nodeport.h
+++ bpf/lib/nodeport.h
@@ -882,7 +882,7 @@ redo:
 
 /* See comment in tail_rev_nodeport_lb4(). */
 static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, int *ifindex,
-					    int *ext_err)
+					    int *ext_err, const bool rev_dnat_at_netdev)
 {
 	int ret, fib_ret, ret2, l3_off = ETH_HLEN, l4_off, hdrlen;
 	struct ipv6_ct_tuple tuple = {};
@@ -910,6 +910,10 @@ static __always_inline int rev_nodeport_lb6(struct __ctx_buff *ctx, int *ifindex
 
 	ret = ct_lookup6(get_ct_map6(&tuple), &tuple, ctx, l4_off, CT_INGRESS, &ct_state,
 			 &monitor);
+
+	/* See ipv4 equivalent for details */
+	if (rev_dnat_at_netdev && !ct_state.node_port)
+		return CTX_ACT_OK;
 
 	if (ret == CT_REPLY && ct_state.node_port == 1 && ct_state.rev_nat_index != 0) {
 		ret2 = lb6_rev_nat(ctx, l4_off, &csum_off, ct_state.rev_nat_index,
@@ -1032,7 +1036,7 @@ int tail_rev_nodeport_lb6(struct __ctx_buff *ctx)
 	 */
 	ctx_skip_host_fw_set(ctx);
 #endif
-	ret = rev_nodeport_lb6(ctx, &ifindex, &ext_err);
+	ret = rev_nodeport_lb6(ctx, &ifindex, &ext_err, false);
 	if (IS_ERR(ret))
 		goto drop;
 	if (!revalidate_data(ctx, &data, &data_end, &ip6))
@@ -1953,7 +1957,7 @@ redo:
  * of the bpf_host, bpf_overlay and of the bpf_lxc.
  */
 static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, int *ifindex,
-					    int *ext_err)
+					    int *ext_err, const bool rev_dnat_at_netdev)
 {
 	struct ipv4_ct_tuple tuple = {};
 	void *data, *data_end;
@@ -2009,6 +2013,14 @@ static __always_inline int rev_nodeport_lb4(struct __ctx_buff *ctx, int *ifindex
 #endif /* ENABLE_EGRESS_GATEWAY */
 	ret = ct_lookup4(get_ct_map4(&tuple), &tuple, ctx, l4_off, CT_INGRESS, &ct_state,
 			 &monitor);
+
+	/* When we do the rev-DNAT for a NodePort reply from a local service endpoint
+	 * on the bpf_host's "to-netdev" instead of bpf_lxc, then this function is
+	 * executed on all packets. Return early if we detect that a packet is not
+	 * the reply to avoid unnecessary waste of resources.
+	 */
+	if (rev_dnat_at_netdev && !ct_state.node_port)
+		return CTX_ACT_OK;
 
 	if (ret == CT_REPLY && ct_state.node_port == 1 && ct_state.rev_nat_index != 0) {
 		reason = TRACE_REASON_CT_REPLY;
@@ -2150,7 +2162,7 @@ int tail_rev_nodeport_lb4(struct __ctx_buff *ctx)
 	 */
 	ctx_skip_host_fw_set(ctx);
 #endif
-	ret = rev_nodeport_lb4(ctx, &ifindex, &ext_err);
+	ret = rev_nodeport_lb4(ctx, &ifindex, &ext_err, false);
 	if (IS_ERR(ret))
 		return send_drop_notify_error_ext(ctx, 0, ret, ext_err,
 						  CTX_ACT_DROP, METRIC_EGRESS);
@@ -2318,5 +2330,37 @@ static __always_inline int handle_nat_fwd(struct __ctx_buff *ctx)
 	return ret;
 }
 
+/* Wrapper function to call rev_nodeport_lb4/6.
+ * This function calls rev_nodeport_lb4/6, to perform conntrack lookup
+ * and reverse DNAT only if it is NodePort traffic.
+ * arguments
+ * ctx    : Pointer to packet context buffer.
+ * return : Returns the output of rev_nodeport_lb4/6 for valid packets.
+ */
+static __always_inline int rev_nodeport_lb(struct __ctx_buff *ctx)
+{
+	int ret = CTX_ACT_OK;
+	int ifindex = 0;
+	int ext_err = 0;
+	__u16 proto;
+
+	if (!validate_ethertype(ctx, &proto))
+		return CTX_ACT_OK;
+	switch (proto) {
+#ifdef ENABLE_IPV4
+	case bpf_htons(ETH_P_IP):
+		ret = rev_nodeport_lb4(ctx, &ifindex, &ext_err, true);
+	break;
+#endif /* ENABLE_IPV4 */
+#ifdef ENABLE_IPV6
+	case bpf_htons(ETH_P_IPV6):
+		ret = rev_nodeport_lb6(ctx, &ifindex, &ext_err, true);
+	break;
+#endif /* ENABLE_IPV6 */
+	default:
+	break;
+	}
+	return ret;
+}
 #endif /* ENABLE_NODEPORT */
 #endif /* __NODEPORT_H_ */
